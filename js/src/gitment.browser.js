2bba4f3505e00270676a173dbe14818b01a3b0c4
wangguocong <871578924@qq.com>
2019-02-26 13:12:12 +0800

    Site updated: 2019-02-26 13:12:11

--- js/src/gitment.browser.js
+++ js/src/gitment.browser.js
@@ -3201,6 +3201,8 @@ function ajaxFactory(method) {
     var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     var base = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'https://api.github.com';
 
+    var headers = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
+
     var req = new XMLHttpRequest();
     var token = localStorage.getItem(_constants.LS_ACCESS_TOKEN_KEY);
 
@@ -3238,6 +3240,11 @@ function ajaxFactory(method) {
     if (method !== 'GET' && method !== 'DELETE') {
       body = JSON.stringify(data);
       req.setRequestHeader('Content-Type', 'application/json');
+    }
+
+    //设置自定义的 header
+    for(let header in headers){
+        req.setRequestHeader([header], headers[header]);
     }
 
     req.send(body);
@@ -3413,11 +3420,11 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://auth.baixiaotu.cc', {
+      _utils.http.post('https://cors.wenjunjiang.win/?remoteUrl=https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
-      }, '').then(function (data) {
+      }, '',{Accept:'application/json'}).then(function (data) {
         _this.accessToken = data.access_token;
         _this.update();
       }).catch(function (e) {
